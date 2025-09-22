a2bf5823f0a378c93eabfa31ffec3fc71597588c
Thomas Rose <thomas.rose@daad-alumni.de>
2025-09-22 07:47:47 +0200

    start adding templates [skip ci]

---
+++ dev_guide/ASTR_function.R
@@ -0,0 +1,32 @@
+
+#' <Title of function>
+#'
+#' @description
+#' < What the function does, 1 or 2 sentences>
+#'
+#' @details
+#' < Any text written here goes into the "Details" section >
+#'
+#'
+#' @param argument1 < Explain type of argument and what it does here. If you set
+#' a default value, give a reason why it is default unless this is obvious.>
+#' @param argument2 integer, <doing stuff>. The default value is 42.
+#' @param ... Further arguments passed to <name functions>.
+#'
+#' @references
+#' < include here any relevant literature references>
+#'
+#' @returns
+#' < Describe the output of the function. This part goes into the "Value" section>
+#'
+#' @export # remove if the function should not be visible to the user
+#'
+#' @examples
+#' <write here any example code. These are small simple examples on how to use
+#' the function or to highlight specific features>
+
+name <- function(argument1, argument2 = 42, ...) { # update name and arguments. The ellipsis parameter is special in R, use with care!
+
+  # any function code goes here.
+
+}
