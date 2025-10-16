e76028ff270d83fc8cc6262a17a2686e9af0aaab
Clemens Schmid <nevromecs@gmail.com>
2025-10-16 15:46:17 +0200

    another attempt

--- tests/testthat/test_archchem_basic.R
+++ tests/testthat/test_archchem_basic.R
@@ -1,6 +1,8 @@
 # golden tests
 
-test_input <- read_archchem("input_format.csv")
+test_input <- read_archchem(
+  system.file("extdata", "input_format.csv", package = "ASTR")
+)
 
 test_that("reading of a basic example table works as expected", {
   expect_snapshot({
