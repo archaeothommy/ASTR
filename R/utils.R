ce66f0d7dc9772a328d283ffede4abb4e9553e9d
marco <marco@footium.club>
2025-10-22 11:51:22 +0200

    added first util functions to filter datasets

---
+++ R/utils.R
@@ -0,0 +1,29 @@
+#' @export
+check_columns_exist <- function(data, columns) {
+  #
+  if (!is.data.frame(data)) {
+    stop("Data needs to be a tibble or data frame!")
+  }
+
+  missing_cols <- setdiff(columns, colnames(data))
+
+  if (length(missing_cols) > 0) {
+    stop(paste("Columns missing in dataset:", paste(missing_cols, collapse = ", ")))
+  }
+
+  return(TRUE)
+}
+
+
+#' @export
+filter_columns_with_id <- function(data, columns) {
+  #all data is supposed to have an id column
+  required_cols <- c("id", columns)
+
+  check_columns_exist(data, required_cols)
+
+  data_filtered <- data[, required_cols]
+
+  return(data_filtered)
+}
+
