5858a3078e2a6f6d3be479c4edbcc595061c1479
Thomas Rose <thomas.rose@daad-alumni.de>
2025-10-23 11:13:30 +0200

    make functions pipe-friendly, consistent styling, improved testing

--- tests/testthat/test_LI_age_models.R
+++ tests/testthat/test_LI_age_models.R
@@ -5,8 +5,10 @@ age_model_data <- tibble::tibble(
 )
 
 test_that("Pb isotope age models", {
-  expect_equal(pb_iso_age_model(age_model_data, model = "SK75")[[1]], -1166.946)
-  expect_equal(pb_iso_age_model(age_model_data, model = "CR75")[[2]], 10.478)
-  expect_equal(pb_iso_age_model(age_model_data, model = "AJ84")[[3]], -15.421)
-  expect_length(pb_iso_age_model(age_model_data, model = "all"), 9)
+  expect_equal(pb_iso_age_model(age_model_data, model = "SK75")[["model_age_SK75"]], -1166.946)
+  expect_equal(pb_iso_age_model(age_model_data, model = "CR75")[["mu_CR75"]], 10.478)
+  expect_equal(pb_iso_age_model(age_model_data, model = "AJ84")[["kappa_AJ84"]], -15.421)
+  expect_error(pb_iso_age_model(age_model_data, model = 23), "Assertion on*")
+  expect_error(pb_iso_age_model(age_model_data, model = "23"), "This model is not supported.")
+  expect_length(pb_iso_age_model(age_model_data, model = "all"), 12)
 })
