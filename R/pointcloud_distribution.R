86ca1f474c2a6d9b0cc496b61da0e188f78074eb
Karan Desai <workforce.karan@gmail.com>
2025-10-24 09:48:07 +0200

    Function example and optemisation done

--- R/pointcloud_distribution.R
+++ R/pointcloud_distribution.R
@@ -19,51 +19,48 @@
 #' @export
 #'
 #' @examples
-#
-# iso <- c("206Pb/204Pb","207Pb/204Pb","208Pb/204Pb")
-#
-# # Create Reference data
-# groups <- LETTERS[1:4]
-# rand_iso <- \(){c(runif(1, 18.3, 19), runif(1, 15.5, 15.9), runif(1, 37.5, 39))}
-# list_df <- lapply(groups, \(x){
-#   ls <- sapply(rand_iso(),  \(g){rnorm(20, g, runif(1, 0.05, 0.1))})
-#   colnames(ls) <- iso
-#   ls <- as.data.frame(ls)
-#   ls$groups <- x
-#   ls
-#   })
-# ref <- as.data.frame(do.call(rbind, list_df))
-#
-# # Create working data
-#
-# wod <- as.data.frame(sapply(rand_iso(),  \(g){rnorm(20, g, 0.1)}))
-# colnames(wod) <- iso
-# wod$samples <- letters[1:20]
-#
-# rm(list_df, iso, groups, rand_iso)
-#
-# plot(ref$`206Pb/204Pb`, ref$`207Pb/204Pb`, col = as.factor(ref$groups))
-# points(wod$`206Pb/204Pb`, wod$`207Pb/204Pb`, pch = 3)
-# pointcloud_distribution(wod, ref)
-
-
-
-####
+#' # Creaete Synthetic Data
+#' iso <- c("206Pb/204Pb","207Pb/204Pb","208Pb/204Pb")
+#' ## Create Synthetic Reference data
+#' groups <- LETTERS[1:4]
+#' rand_iso <- \(){c(runif(1, 18.3, 19), runif(1, 15.5, 15.9), runif(1, 37.5, 39))}
+#' list_df <- lapply(groups, \(x){
+#'   ls <- sapply(rand_iso(),  \(g){stats::rnorm(20, g, stats::runif(1, 0.05, 0.1))})
+#'   colnames(ls) <- iso
+#'   ls <- as.data.frame(ls)
+#'   ls$groups <- x
+#'   ls
+#' })
+#' ref <- as.data.frame(do.call(rbind, list_df))
+#' ## Create working data
+#' wod <- as.data.frame(sapply(rand_iso(),  \(g){rnorm(20, g, 0.1)}))
+#' colnames(wod) <- iso
+#' wod$samples <- letters[1:20]
+#' rm(list_df, iso, groups, rand_iso)
+#' # Run Pointcloud_distribution
+#' pointcloud_distribution(ref, "sample", ref, "groups")
 pointcloud_distribution <- function(wod,
                                      samples = "samples",
                                      ref,
                                      groupby = "groups") {
+  # Get iso names from Wod
+  wod_names <- grep("204", names(wod), value = TRUE)
+  # Get iso names for Ref
+  ref_names <- grep("204", names(ref), value = TRUE)
+
   # Get working data list
-  wod_points <- wod[grep("204", names(wod))]
+  wod_points <- wod[wod_names]
+  # Get Ref data list
+  ref <- ref[,c(groupby, ref_names)]
   # Finds hull for the ref Data
-  ref_hull <- geometry::convhulln(ref[grep("204", names(ref))])
+  ref_hull <- geometry::convhulln(ref[ref_names])
   # Checks if working points are within the hull
   hull_inout <- geometry::inhulln(ref_hull, as.matrix(wod_points))
   # Identifes unique group names
   groups <- unique(ref[[groupby]])
   # Finds out if the points are within the hull of each indificual group
   group_inout <- sapply(groups, \(i) {
-    t <- geometry::convhulln(ref[ref[[groupby]] == i, grep("204", names(ref))])
+    t <- geometry::convhulln(ref[ref[[groupby]] == i, ref_names])
     geometry::inhulln(t, as.matrix(wod_points))
   })
   # Combines total and individual in and out logic
@@ -74,7 +71,7 @@ pointcloud_distribution <- function(wod,
   formula_srt <- paste0(".~`", groupby, "`")
   ref_centroids <- aggregate(as.formula(formula_srt), ref, mean)
   # Calculate distance of each point from centroides
-  distances <- rdist::cdist(wod[-1], ref_centroids[-1])
+  distances <- rdist::cdist(wod[wod_names], ref_centroids[grep("204", names(ref_centroids))]) # Correct this.
   # Rename distance table rows and columns
   rownames(distances) <- wod[[samples]]
   colnames(distances) <- ref_centroids[[groupby]]
@@ -87,3 +84,8 @@ pointcloud_distribution <- function(wod,
   # Returns Output.
   return(output)
 }
+
+
+
+
+
