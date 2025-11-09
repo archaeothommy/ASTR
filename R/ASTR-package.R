8657974049c864459de091fdd56c0f6113056a4e
Thomas Rose <55918349+archaeothommy@users.noreply.github.com>
2025-11-09 22:30:44 +0100

    Revert "mixing model (#14)"
    This reverts commit 2390ebc12fde42f24d112ce990c01f2e7a103b8e.

--- R/ASTR-package.R
+++ R/ASTR-package.R
@@ -2,10 +2,5 @@
 "_PACKAGE"
 
 ## usethis namespace: start
-#' @importFrom geometry convhulln
-#' @importFrom geometry inhulln
-#' @importFrom rdist cdist
-#' @importFrom stats aggregate
-#' @importFrom stats as.formula
 ## usethis namespace: end
 NULL
