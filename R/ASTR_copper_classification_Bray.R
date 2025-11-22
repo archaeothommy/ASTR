0bd63a8b6620881fd1382ff8067466a499562e03
Thomas Rose <thomas.rose@daad-alumni.de>
2025-11-22 14:37:09 +0100

    update tests and documentation

--- R/ASTR_copper_classification_Bray.R
+++ R/ASTR_copper_classification_Bray.R
@@ -1,23 +1,27 @@
-#' Classify copper samples into compositional groups (Bray et al., 2015)
+#' Copper classification according to Bray et al. (2015)
 #'
-#' @description
-#' Classifies copper artefacts into one of the 16 trace-element
-#' compositional groups defined by Bray et al., 2015 (Fig. 1),
-#' based on the presence/absence of As, Sb, Ag, and Ni.
+#' @description Classification of copper artefacts according to Bray et al. (2015) into
+#' one of 16 trace element compositional groups based on As, Sb, Ag, or
+#' Ni being below or above 0.1 wt%.
 #'
-#' @param df A dataframe containing at least columns: ID, As, Sb, Ag, Ni.
+#' @references Bray, P., Cuénod, A., Gosden, C., Hommel, P., Liu, P. and
+#'   Pollard, A. M. (2015), Form and flow: the ‘karmic cycle’ of copper. Journal
+#'   of Archaeological Science 56, pp. 202-209.
+#'   <https://doi.org/10.1016/j.jas.2014.12.013>.
+#'
+#' @param df data frame with the data to be classified.
+#' @param elements Named character vector with the column names of the As, Sb, Ag, and Ni concentrations.
 #' @param threshold Numeric. Values > threshold are interpreted as "present".
-#' Default is 0.1 (as suggested in Bray et al., 2015).
+#'   Default is 0.1 (as suggested in Bray et al., 2015).
 #'
-#' @return The original dataframe with one new column:
-#' \code{copper_group_bray}
+#' @return The original data frame with the added column `copper_group_bray`.
 #'
 #' @export
-classify_copper_group_bray <- function(df, threshold = 0.1) {
+copper_group_bray <- function(df, elements = c(As = "As", Sb = "Sb", Ag = "Ag", Ni = "Ni"), threshold = 0.1) {
   # Build a temporary classification table
   temp <- data.frame(
     ID = df$ID,
-    group = dplyr::case_when(
+    copper_group_bray = dplyr::case_when(
       df$As <= threshold & df$Sb <= threshold & df$Ag <= threshold & df$Ni <= threshold ~ "None",
       df$As > threshold & df$Sb <= threshold & df$Ag <= threshold & df$Ni <= threshold ~ "As",
       df$As <= threshold & df$Sb > threshold & df$Ag <= threshold & df$Ni <= threshold ~ "Sb",
@@ -39,7 +43,6 @@ classify_copper_group_bray <- function(df, threshold = 0.1) {
   )
   # Merge back into the original dataframe without altering it
   df <- dplyr::left_join(df, temp, by = "ID")
-  # Clean column name to publication-specific name
-  names(df)[names(df) == "group"] <- "copper_group_bray"
+
   return(df)
 }
