c964fe70ae6e2c4c4d0734f45555528cf85eb880
Thomas Rose <roseth@posteo.com>
2026-01-31 15:29:34 -0500

    add example

--- R/Atomic_conversion.R
+++ R/Atomic_conversion.R
@@ -3,8 +3,8 @@
 #' Convert chemical compositions between weight percent (wt%) and atomic percent
 #' (at%).
 #'
-#' The column names of the elements to be converted must be equivalent to
-#' their chemical symbols. Results are always normalized to 100%.
+#' The column names of the elements to be converted must be equivalent to their
+#' chemical symbols. Results are always normalised to 100%.
 #'
 #' @param df Data frame with compositional data.
 #' @param elements character vector with the chemical symbols of the elements
@@ -15,15 +15,20 @@
 #'   * `_at` for conversions to atomic percent
 #'   * `_wt` for conversions to weight percent.
 #'
-#' @return Data frame with the converted concentrations normalized to 100%.
+#' @return The original data frame with the converted concentrations normalised
+#'   to 100%.
 #'
 #' @export
 #' @name atomic_conversion
 #'
 #' @examples
-#' # Convert weight percent to atomic percent
+#' # Convert weight percent to atomic percent and to weight percent
 #' df <- data.frame(Si = 46.74, O = 53.26)  # SiO2 composition
-#' wt_to_at(df, elements = c("Si", "O"))
+#' at <- wt_to_at(df, elements = c("Si", "O"))
+#' at_to_wt(at, elements = c("Si", "O"))
+#'
+#' # preserve columns with unconverted values
+#' wt_to_at(df, elements = c("Si", "O"), drop = FALSE)
 #'
 wt_to_at <- function(df, elements, drop = TRUE) {
 
