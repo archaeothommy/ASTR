b33069b50e909f8fcec222037c903dffeca92fe9
Thomas Rose <thomas.rose@daad-alumni.de>
2026-02-16 18:05:49 -0500

    make created plot reprodubile

--- tests/testthat/test_geom_kde2.R
+++ tests/testthat/test_geom_kde2.R
@@ -1,5 +1,6 @@
 # Visual tests ------------------------------------------------------------
 
+set.seed(50)
 test_KDE_df <- data.frame(
   x = c(rnorm(50), rnorm(50, 5), rnorm(2, 10)),
   y = c(rnorm(50), rnorm(50, 5), rnorm(2, 10)),
