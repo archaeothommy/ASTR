00d99b9ca8b08654e4f38c8c50bec8668d8f9a9b
Clemens Schmid <nevromecs@gmail.com>
2026-02-24 08:14:39 +0100

    satisfied lintr

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,18 @@
 # defining global variables
-# ugly solution to avoid magrittr NOTE
-# see http://stackoverflow.com/questions/9439256/
-# how-can-i-handle-r-cmd-check-no-visible-binding-for-global-variable-notes-when
-globalVariables(".")
+utils::globalVariables(
+  c(
+    # ugly solution to avoid magrittr NOTE
+    # see http://stackoverflow.com/questions/9439256/
+    # how-can-i-handle-r-cmd-check-no-visible-binding-for-global-variable-notes-when
+    ".",
+    # register data objects to make lintr aware of them
+    "isotopes_data",
+    "elements_data",
+    "oxides_data",
+    "special_oxide_states",
+    "conversion_oxides"
+  )
+)
 
 #' @importFrom magrittr "%>%"
 #' @importFrom rlang .data :=
