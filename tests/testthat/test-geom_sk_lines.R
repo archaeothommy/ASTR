4c67520638c3538268cb08ebbe270ed367f8b1ec
Karan Desai <workforce.karan@gmail.com>
2026-03-22 12:36:59 +0200

    bug fix

---
+++ tests/testthat/test-geom_sk_lines.R
@@ -0,0 +1,27 @@
+set.seed(50)
+df <- data.frame(
+  pb64 = rnorm(10, 18, 0.2),
+  pb74 = rnorm(10, 15.7, 0.1),
+  pb84 = rnorm(10, 37.5, 0.1)
+)
+
+test_that("geom_image", {
+  expect_doppelganger(
+    "image",
+    ggplot(df, aes(x = pb64, y = pb74)) +
+      geom_point() +
+      geom_sk_lines(system = "76", show_geochron = FALSE) +
+      geom_sk_lines(system = "76", show_isochrons = FALSE,
+                    color = 'red', linetype = 'dashed') +
+      geom_sk_labels(system = "76", show_geochron  = FALSE) +
+      geom_sk_labels(system = "76", show_isochrons = FALSE, color = 'red') +
+      coord_cartesian(
+        xlim = range(df$pb64) * c(.99, 1.01),
+        ylim = range(df$pb74) * c(.99, 1.01)
+      ) +
+      labs(
+        x = expression({}^206*Pb / {}^204*Pb),
+        y = expression({}^207*Pb / {}^204*Pb),
+      )
+  )
+})
